5b91e95924e8c1f3f5613b0fe9d6a059bca9ab72
Katty57 <kate.tyan57@gmail.com>
2021-11-15 22:09:06 +0600

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by  User on 15.11.2021.
+//  Copyright © 2021 abr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
